bde16f2589fd5434cb50ff36d9ce3ac550ba40ce
Ian144 <ian144@hotmail.com>
2020-12-29 16:59:02 +0000

    added Roslyn generator

--- SortFuncGeneration/Benchmarks.cs
+++ SortFuncGeneration/Benchmarks.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.IO;
 using System.Linq;
 using System.Runtime.CompilerServices;
@@ -20,24 +21,25 @@ namespace SortFuncGeneration
             new SortDescriptor(true, "StrProp1"),
             new SortDescriptor(true, "IntProp2"),
             new SortDescriptor(true, "StrProp2"),
-        };        
+        };
 
-        private static readonly Func<Target, Target, int>[] _composedSubFuncs = { CmpIntProp1, CmpStrProp1, CmpIntProp2, CmpStrProp2 };        
-        
+        private static readonly Func<Target, Target, int>[] _composedSubFuncs = { CmpIntProp1, CmpStrProp1, CmpIntProp2, CmpStrProp2 };
+
         private static List<Target> _source;
         private static List<Target> _sortTargets;
-        
+
         private static readonly Consumer _consumer = new();
 
-        private static readonly InlineComparer _inlineComparer                       = new ();
-        private static readonly IntInlineComparer _intInlineComparer                 = new ();
-        private static readonly ComparerAdaptor<Target> _handCodedTernary            = new( HandCodedTernary );
-        private static readonly ComparerAdaptor<Target> _ilEmittedComparer           = new( ILEmitGenerator.EmitSortFunc<Target>(_sortBys) );
-        private static readonly ComparerAdaptor<Target> _generatedComparer           = new( ExprTreeSortFuncCompiler.MakeSortFunc<Target>(_sortBys) );
-        private static readonly ComparerAdaptor<Target> _handCoded                   = new( HandCoded );
-        private static readonly ComparerAdaptor<Target> _intInlineHandCoded          = new( IntInline );       
-        private static readonly ComparerAdaptor<Target> _composedFunctionsComparer   = new( ComposedFuncs );
-        private static readonly ComparerAdaptor<Target> _combinatorFunctionsComparer = new( CombineFuncs(_composedSubFuncs) );
+        private static readonly InlineComparer _inlineComparer = new();
+        private static readonly IntInlineComparer _intInlineComparer = new();
+        private static readonly ComparerAdaptor<Target> _handCodedTernary = new(HandCodedTernary);
+        private static readonly ComparerAdaptor<Target> _ilEmittedComparer = new(ILEmitGenerator.EmitSortFunc<Target>(_sortBys));
+        private static readonly ComparerAdaptor<Target> _generatedComparer = new(ExprTreeSortFuncCompiler.MakeSortFunc<Target>(_sortBys));
+        private static readonly ComparerAdaptor<Target> _handCoded = new(HandCoded);
+        private static readonly ComparerAdaptor<Target> _intInlineHandCoded = new(IntInline);
+        private static readonly ComparerAdaptor<Target> _composedFunctionsComparer = new(ComposedFuncs);
+        private static readonly ComparerAdaptor<Target> _combinatorFunctionsComparer = new(CombineFuncs(_composedSubFuncs));
+        private IComparer<Target> _roslynComparer;
 
         private static readonly IComparer<Target> _nitoComparer =
             ComparerBuilder.For<Target>()
@@ -51,6 +53,13 @@ namespace SortFuncGeneration
         [GlobalSetup]
         public void Setup()
         {
+            var rosGen = new RoslynGenerator();
+            var sw = Stopwatch.StartNew();
+            _roslynComparer = rosGen.GenComparer();
+            sw.Stop();
+            var elapsed = sw.Elapsed;
+            Console.WriteLine($"############## roslyn compilation took: {elapsed}");
+
             var dir = Path.Combine(Path.GetTempPath(), "targetData.data");
             var fs = new FileStream(dir, FileMode.Open, FileAccess.Read);
             _source = ProtoBuf.Serializer.Deserialize<List<Target>>(fs);
@@ -67,7 +76,7 @@ namespace SortFuncGeneration
                 .ThenBy(x => x.IntProp2)
                 .ThenBy(x => x.StrProp2, StringComparer.Ordinal);
         }
-        
+
         [IterationSetup]
         public void ISetup()
         {
@@ -76,7 +85,7 @@ namespace SortFuncGeneration
             {
                 _sortTargets[ctr] = _source[ctr];
             }
-        }        
+        }
 
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
         private static int CmpIntProp1(Target p1, Target p2) => p1.IntProp1.CompareTo(p2.IntProp1);
@@ -139,24 +148,24 @@ namespace SortFuncGeneration
                         : CompareOrdinal(xx.StrProp2, aa.StrProp2);
         }
 
-        
+
         private static int IntInline(Target xx, Target yy)
         {
             int xxIp1 = xx.IntProp1;
             int aaIp1 = yy.IntProp1;
             if (xxIp1 < aaIp1) return -1;
             if (xxIp1 > aaIp1) return 1;
-           
+
             int tmp = CompareOrdinal(xx.StrProp1, yy.StrProp1);
-            if(tmp != 0)
+            if (tmp != 0)
                 return tmp;
 
             int xxIp2 = xx.IntProp2;
             int aaIp2 = yy.IntProp2;
             if (xxIp2 < aaIp2) return -1;
             return xxIp2 > aaIp2 ? 1 : CompareOrdinal(xx.StrProp2, yy.StrProp2);
-        }        
-        
+        }
+
         public bool IsValid()
         {
             Setup();
@@ -168,8 +177,8 @@ namespace SortFuncGeneration
             var composedFunctionsSorted = _source.OrderBy(m => m, _composedFunctionsComparer);
             var combinatorFunctionsSorted = _source.OrderBy(m => m, _combinatorFunctionsComparer);
             var handCodedTernarySorted = _source.OrderBy(m => m, _handCodedTernary);
-            var inlineSorted = _source.OrderBy(m => m, _inlineComparer );
-            var intInlineSorted = _source.OrderBy(m => m, _intInlineComparer );
+            var inlineSorted = _source.OrderBy(m => m, _inlineComparer);
+            var intInlineSorted = _source.OrderBy(m => m, _intInlineComparer);
             var genEmitSorted = _source.OrderBy(m => m, _ilEmittedComparer);
 
             //bool b1 = referenceOrdering.SequenceEqual(genSorted);
@@ -191,6 +200,9 @@ namespace SortFuncGeneration
         }
 
         [Benchmark]
+        public void RoslynGenerated() => _sortTargets.Sort(_roslynComparer);
+
+        [Benchmark]
         public void ExprTreeGenerated() => _sortTargets.Sort(_generatedComparer);
 
         [Benchmark]
@@ -204,25 +216,25 @@ namespace SortFuncGeneration
 
         [Benchmark]
         public void HandCodedTernary() => _sortTargets.Sort(_handCodedTernary);
-        
+
         [Benchmark]
         public void IntInlineComparer() => _sortTargets.Sort(_intInlineComparer);
 
         [Benchmark]
-        public void InlineComparer() => _sortTargets.Sort(_inlineComparer);        
-        
+        public void InlineComparer() => _sortTargets.Sort(_inlineComparer);
+
         [Benchmark]
         public void HandCoded() => _sortTargets.Sort(_handCoded);
-        
+
         [Benchmark]
-        public void IntInlineHandCoded() => _sortTargets.Sort(_intInlineHandCoded);        
+        public void IntInlineHandCoded() => _sortTargets.Sort(_intInlineHandCoded);
 
         [Benchmark]
         public void Nito() => _sortTargets.Sort(_nitoComparer);
-        
+
         [Benchmark]
         public void ExprTreeGeneratedOrderBy() => _sortTargets.OrderBy(m => m, _generatedComparer).Consume(_consumer);
-        
+
         [Benchmark]
         public void LinqBaseLine() => _lazyLinqOrderByThenBy.Consume(_consumer);
     }
---
+++ SortFuncGeneration/RoslynGenerator.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp;
+
+namespace SortFuncGeneration
+{
+    public class RoslynGenerator
+    {
+        private readonly IList<MetadataReference> _references = new List<MetadataReference>();
+
+        public RoslynGenerator()
+        {
+            ReferenceAssemblyContainingType<object>();
+            ReferenceAssembly( typeof(Enumerable).Assembly);
+            
+            // reference the assembly containing type SortFuncGeneration.Target, i.e. this assembly
+            ReferenceAssembly(Assembly.GetExecutingAssembly());
+        }
+
+        private void ReferenceAssembly(Assembly assembly) => _references.Add(MetadataReference.CreateFromFile(assembly.Location));
+
+        private void ReferenceAssemblyContainingType<T>() => ReferenceAssembly(typeof (T).Assembly);
+
+        private  Assembly GenerateAssembly(string code)
+        {
+            var assemblyName = Path.GetRandomFileName();
+            var syntaxTree = CSharpSyntaxTree.ParseText(code);
+
+            var compilation = CSharpCompilation.Create(
+                assemblyName, 
+                new[]{syntaxTree}, 
+                _references,
+                new CSharpCompilationOptions(OutputKind.DynamicallyLinkedLibrary));
+
+            using var stream = new MemoryStream();
+            var result = compilation.Emit(stream);
+
+            if (!result.Success)
+            {
+                IEnumerable<string> messages = result.Diagnostics.Select(x => $"{x.Id}: {x.GetMessage()}");
+                string msgStr = string.Join('\n', messages);
+                throw new InvalidOperationException(msgStr);
+            }
+
+            stream.Seek(0, SeekOrigin.Begin);
+            return Assembly.Load(stream.ToArray());
+        }
+
+        public IComparer<Target> GenComparer()
+        {
+            var code = @"using SortFuncGeneration;
+                                 
+                        namespace RosGen
+                        {
+                             public class IntInlineComparer : System.Collections.Generic.IComparer<Target>
+                             {
+                                 int System.Collections.Generic.IComparer<Target>.Compare(Target xx, Target yy)
+                                 {
+                                     if (xx.IntProp1 < yy.IntProp1) return -1;
+                                     if (xx.IntProp1 > yy.IntProp1) return 1;
+                         
+                                     int tmp = string.CompareOrdinal(xx.StrProp1, yy.StrProp1);
+                                     if (tmp != 0)
+                                         return tmp;
+                         
+                                     if (xx.IntProp2 < yy.IntProp2) return -1;
+                                     return xx.IntProp2 > yy.IntProp2 
+                                         ? 1 
+                                         : string.CompareOrdinal(xx.StrProp2, yy.StrProp2);
+                                 } 
+                             }
+                        }";
+
+            var assemblyName = Path.GetRandomFileName();
+            var syntaxTree = CSharpSyntaxTree.ParseText(code);
+
+            var compilation = CSharpCompilation.Create(
+                assemblyName, 
+                new[] {syntaxTree}, 
+                _references,
+                new CSharpCompilationOptions( OutputKind.DynamicallyLinkedLibrary,  optimizationLevel: OptimizationLevel.Release));
+
+            using var stream = new MemoryStream();
+            var result = compilation.Emit(stream);
+
+            if (!result.Success)
+            {
+                IEnumerable<string> messages = result.Diagnostics.Select(x => $"{x.Id}: {x.GetMessage()}");
+                string msgStr = string.Join('\n', messages);
+                throw new InvalidOperationException(msgStr);
+            }
+
+            stream.Seek(0, SeekOrigin.Begin);
+            var assembly = Assembly.Load(stream.ToArray());
+            Type type = assembly.GetType("RosGen.IntInlineComparer");
+            IComparer<Target> comp = (IComparer<Target>) Activator.CreateInstance(type) ;
+            return comp;
+        }
+    }
+}
